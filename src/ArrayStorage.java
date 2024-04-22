2ff53419d47230119bddeebdf777cfdff5420e26
O.Nes <webcom03@gmail.com>
2024-04-22 21:16:47 +0300

    refactoring array size method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -61,12 +61,7 @@ public class ArrayStorage {
     }
 
     int size() {
-        int countResume = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                countResume++;
-            }
-        }
-        return countResume;
+        int size = getAll().length;
+        return size;
     }
 }
